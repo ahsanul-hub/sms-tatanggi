94f44043f7660a916a3f394ab304e8254a013acf
Apple <aldi.madridista.am@gmail.com>
2025-09-26 11:13:45 +0700

    fix(/): add config vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
   },
-}
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push("@prisma/client");
+    }
+    return config;
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
